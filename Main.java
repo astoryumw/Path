7a5b18db4d655511e42cfd88893cff0d12fc1f03
Austin Story <storyaustin25@gmail.com>
2023-09-28 11:19:12 -0400

    Finished basic program where it goes through the map and finds the opposing 2. Program needs to be cleared up.

--- Main.java
+++ Main.java
@@ -18,6 +18,149 @@ public class Main {
         
         m.fileWriter(myArray);
         // System.out.println(Arrays.toString(myArray[0]));
+
+        if (m.processer(myArray)) {
+            System.out.println("There is a path!");
+        }
+    }
+
+    // new idea: use connection to call where it is and find the next piece
+    // depending on direction, have a different method for each that is called
+    // method returns final area
+    public boolean processer(int[][] myArray) {
+        Main m = new Main();
+        int location = 0, height = 0;
+        int[] afterRun = new int[2];
+        int lastNumber = 0;
+        // find the initial 2
+        location = Arrays.binarySearch(myArray[0],2);
+        // System.out.println(location);
+        if (myArray[0][location-1]==1) {
+            System.out.println(Arrays.toString(m.goLeft(myArray, 0, location)));
+            afterRun = m.goLeft(myArray, 0, location);
+            System.out.println(Arrays.toString(afterRun));
+            lastNumber = myArray[afterRun[0]][afterRun[1]];
+        } else if (myArray[0][location+1]==1) {
+            System.out.println(Arrays.toString(m.goRight(myArray, 0, location)));
+            afterRun = m.goLeft(myArray, 0, location);
+            System.out.println(Arrays.toString(afterRun));
+            lastNumber = myArray[afterRun[0]][afterRun[1]];
+        } else if (myArray[1][location]==1) {
+            System.out.println(Arrays.toString(m.goDown(myArray, 1, location)));
+            afterRun = m.goLeft(myArray, 1, location);
+            System.out.println(Arrays.toString(afterRun));
+            lastNumber = myArray[afterRun[0]][afterRun[1]];
+        }
+        System.out.println(Arrays.toString(afterRun));
+        System.out.println(myArray[afterRun[0]][afterRun[1]]);
+        System.out.println(lastNumber);
+        height = afterRun[0];
+        System.out.println("Height: " + height);
+        location = afterRun[1];
+        System.out.println("Location: " + location);
+        int left = 0, right = 0, down = 0;
+        boolean goingLeft = false, goingRight = false, goingDown = false;;
+        // keep going until the 2 is found, eventually will need it to return false if it cannot be found
+        while (lastNumber!=2) {
+            System.out.println(myArray[height][location-1]);
+            if (myArray[height][location-1]==1 && !goingRight) {
+                System.out.println("Go left.");
+                goingLeft = true;
+                goingRight = false;
+                goingDown = false;
+                // System.out.println(Arrays.toString(m.goLeft(myArray, afterRun[0], afterRun[1])));
+                afterRun = m.goLeft(myArray, height, location);
+                System.out.println(Arrays.toString(afterRun));
+                location = afterRun[1];
+                lastNumber = myArray[afterRun[0]][afterRun[1]];
+            } else if (myArray[height][location+1]==1 && !goingLeft) {
+                System.out.println("Go right.");
+                goingLeft = false;
+                goingRight = true;
+                goingDown = false;
+                // System.out.println(Arrays.toString(m.goRight(myArray, afterRun[0], afterRun[1])));
+                afterRun = m.goLeft(myArray, afterRun[0], afterRun[1]);
+                System.out.println(Arrays.toString(afterRun));
+                location = afterRun[1];
+                lastNumber = myArray[afterRun[0]][afterRun[1]];
+            } else if (myArray[height+1][location]==1) {
+                goingLeft = false;
+                goingRight = false;
+                goingDown = true;
+                System.out.println("Go down.");
+                System.out.println(height + " " + location);
+                // System.out.println(Arrays.toString(m.goDown(myArray, height, location)));
+                afterRun = m.goDown(myArray, height, location);
+                System.out.println(Arrays.toString(afterRun));
+                height = afterRun[0];
+                lastNumber = myArray[afterRun[0]][afterRun[1]];
+                // down++;
+            }
+            // break;
+        }
+        System.out.println(lastNumber);
+        if (lastNumber==2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // needs to return new place in myArray
+    // if im going left, could use binarySearch to find the first 1, which would be at the end of the list
+    public int[] goLeft(int[][] myArray, int height, int location) {
+        // System.out.println("1");
+        int[] newArray = new int[2];
+        for (int i=location; i>=0; i--) {
+            System.out.println(myArray[height][i]);
+            if (myArray[height][i]==0) {
+                // System.out.println("1");
+                newArray[0] = height;
+                newArray[1] = i+1;
+                // System.out.println(newArray);
+                return newArray; // return height, location
+            } else if (myArray[height][i]==2) {
+                newArray[0] = height;
+                newArray[1] = i;
+                return newArray;
+            }
+        }
+        System.out.println("Returning null");
+        return null;
+    }
+
+    // to go right, find first 0 and then go back, but it must go past the initial 0s
+    public int[] goRight(int[][] myArray, int height, int location) {
+        int[] newArray = new int[2];
+        for (int i=location; i<=myArray[height].length; i++) {
+            // System.out.println(myArray[height][i]);
+            if (myArray[height][i]==0) {
+                newArray[0] = height;
+                newArray[1] = i-1;
+                return newArray;
+            }
+        }
+        return null;
+    }
+
+    public int[] goDown(int[][] myArray, int height, int location) {
+        int[] newArray = new int[2];
+        for (int i=height; i<=myArray.length; i++) {
+            // System.out.println(myArray[i][location]);
+            try {
+                if (myArray[i][location]==0) { // getting caught here
+                    System.out.println("Done");
+                    newArray[0] = i-1;
+                    newArray[1] = location;
+                    return newArray;
+                }
+            } catch (ArrayIndexOutOfBoundsException aioobe) {
+                newArray[0] = i-1;
+                newArray[1] = location;
+                return newArray;
+            }
+        }
+        return null;
     }
 
     // need method that goes through the maze and see's if there is a way
@@ -29,20 +172,20 @@ public class Main {
             if (myArray[0][i]==2) {
                 // go horizontal, thisll send it to the left
                 for (int u=myArray[0].length-1; u>=0; u--) {
-                    System.out.println(myArray[0][u]);
+                    // System.out.println(myArray[0][u]);
                     if (myArray[0][u-1]==0) {
                         for (int x=0; x<myArray.length; x++) {
-                            System.out.println(myArray[x][u]);
+                            // System.out.println(myArray[x][u]);
                             if (myArray[x+1][u]==0) {
                                 for (int e=u; e>=0; e--) {
-                                    System.out.println(myArray[x][e]);
+                                    // System.out.println(myArray[x][e]);
                                     if (myArray[x][e-1]==0) {
                                         for (int y=x; y<myArray.length; y++) {
-                                            System.out.println(myArray[y][e] + "teehee" + y + myArray.length);
+                                            // System.out.println(myArray[y][e] + "teehee" + y + myArray.length);
                                             if (/*myArray[y+1][e]==0 || */y==myArray.length-1) { // for some reason the first statement made it crash
                                                 for (int z=e; z>=0; z--) {
                                                     if (myArray[y][z]==2) {
-                                                        System.out.println("EOL");
+                                                        // System.out.println("EOL");
                                                         return true;
                                                     }
                                                 }
@@ -54,14 +197,6 @@ public class Main {
                         }
                     }
                 }
-                // go vertical, send it down
-                // for (int x=0; x<myArray.length; x++) {
-                //     System.out.println(myArray[x][myArray.length-1]);
-                //     if (myArray[x+1][myArray.length-1]==0) {
-                //         System.out.println("EOL");
-                //         break;
-                //     }
-                // }
             }
         }
 
